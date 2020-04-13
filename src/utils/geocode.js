6ac833f8710daf3ad6113c1cd5787723d04ff6cc
Kirti Desai <kirtikd19@gmail.com>
2020-04-13 15:59:25 -0400

    Initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZGVzYWlraXJ0aSIsImEiOiJjazhxYzcybHMwMmVuM2ZwaGRpemFoN2FzIn0.rWxrJ3hyFXXJxG83f8_CGA'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
